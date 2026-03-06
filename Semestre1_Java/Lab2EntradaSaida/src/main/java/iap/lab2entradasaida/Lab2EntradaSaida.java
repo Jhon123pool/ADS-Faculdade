7cff673fa171bbe6dc38f56bd19e2a14c8a94f03
João Vitor Nascimento <jvjndesbrava2017@hotmail.com>
2026-03-06 15:10:22 -0300

    Update ADS $(date +%d/%m/%Y)

---
+++ Semestre1_Java/Lab2EntradaSaida/src/main/java/iap/lab2entradasaida/Lab2EntradaSaida.java
@@ -0,0 +1,7 @@
+package iap.lab2entradasaida;
+
+public class Lab2EntradaSaida {
+    public static void main(String[] args) {
+        System.out.println("🚀 Projeto Lab2EntradaSaida iniciado no pacote iap.lab2entradasaida!");
+    }
+}
