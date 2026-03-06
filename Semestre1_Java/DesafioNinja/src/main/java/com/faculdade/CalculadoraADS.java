aae87c8670c85816cdf6b598fe3fa1e651b8fa1b
João Vitor Nascimento <jvjndesbrava2017@hotmail.com>
2026-03-06 14:24:17 -0300

    Update ADS $(date +%d/%m/%Y)

---
+++ Semestre1_Java/DesafioNinja/src/main/java/com/faculdade/CalculadoraADS.java
@@ -0,0 +1,7 @@
+package com.faculdade;
+
+public class CalculadoraADS {
+    public static void main(String[] args) {
+        System.out.println("O modo Ninja funcionou na CalculadoraADS!");
+    }
+}
