97dabf1bc48be72779cf6415319f7600e22f1b53
João Vitor Nascimento <jvjndesbrava2017@hotmail.com>
2026-03-09 22:32:06 -0300

    Update ADS $(date +%d/%m/%Y)

--- Semestre1_Java/Lab2EntradaSaida/src/main/java/iap/lab2entradasaida/Lab2EntradaSaida.java
+++ Semestre1_Java/Lab2EntradaSaida/src/main/java/iap/lab2entradasaida/Lab2EntradaSaida.java
@@ -1,7 +1,30 @@
 package iap.lab2entradasaida;
+import java.util.Scanner; /* A ferramenta Scanner deve ser importada no início
+                           * do código, antes do método main, ela serve para
+                           * ler a entrada (teclado) do usuário. */
 
 public class Lab2EntradaSaida {
     public static void main(String[] args) {
-        System.out.println("🚀 Projeto Lab2EntradaSaida iniciado no pacote iap.lab2entradasaida!");
+        Scanner entrada = new Scanner (System.in); /* Aqui a gente define onde o
+                                                    * Scanner vai buscar os dados
+                                                    * de entrada do usuário. */
+
+        System.out.println("Digite um número inteiro: ");
+        int numeroInteiro = entrada.nextInt (); /* Aqui a gente define uma caixa onde
+                                                 * um número inteiro digitado pelo
+                                                 * usuário será armazenado. */
+        System.out.println("Digite outro número inteiro: ");
+        int numeroInteiro2 = entrada.nextInt ();
+        int soma = numeroInteiro + numeroInteiro2; /* Aqui a gente define mais uma
+                                                    * caixa e atribuímos uma 
+                                                    * operação de soma com os números
+                                                    * armazenados anteriormente. */
+
+        System.out.printf("Resultado: %d", soma); /* Por fim, aqui nós exibimos o
+                                                   * resultado da operção de soma entre
+                                                   * os dois números que foram armazenados
+                                                   * e somados na terceira caixa utilizando
+                                                   * o printf e o %d que preparam saída para
+                                                   * exibir números. */
     }
 }
