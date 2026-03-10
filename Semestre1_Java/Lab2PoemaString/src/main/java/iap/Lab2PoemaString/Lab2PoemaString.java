91402c72e22aeb5494d07abd15abd0363b87f05a
João Vitor Nascimento <jvjndesbrava2017@hotmail.com>
2026-03-09 22:59:00 -0300

    Update ADS $(date +%d/%m/%Y)

---
+++ Semestre1_Java/Lab2PoemaString/src/main/java/iap/Lab2PoemaString/Lab2PoemaString.java
@@ -0,0 +1,12 @@
+package iap.Lab2PoemaString;
+
+public class Lab2PoemaString {
+    public static void main(String[] args) {
+        String acao = "canto";
+        System.out.println("Eu " + acao + " porque o instante existe");
+        System.out.println("e a minha vida está completa.");
+        System.out.println("Não sou alegre nem sou triste:");
+        String identidade = "sou poeta";
+        System.out.println(identidade + ".");
+    }
+}
