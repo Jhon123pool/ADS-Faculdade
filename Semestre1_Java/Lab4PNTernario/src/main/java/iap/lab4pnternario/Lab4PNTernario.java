3351e16a452b8a021fa03c77ae6af9bbd90d5ac2
João Vitor Nascimento <jvjndesbrava2017@hotmail.com>
2026-03-24 16:12:32 -0300

    Update ADS $(date +%d/%m/%Y)

---
+++ Semestre1_Java/Lab4PNTernario/src/main/java/iap/lab4pnternario/Lab4PNTernario.java
@@ -0,0 +1,29 @@
+package iap.lab4pnternario;
+// Importa o Scanner, que lê o input do usuário.
+import java.util.Scanner;
+
+public class Lab4PNTernario {
+    public static void main(String[] args) {
+        // Cria um novo objeto scanner com o nome 'entrada'.
+        Scanner entrada = new Scanner(System.in);
+
+        /** 
+         * Exibe uma mensagem ao usuário pedindo um número inteiro para ser verificado;
+         * Em seguida armazena o número digitado na variável 'numero'.
+         */
+        System.out.print("Digite um número inteiro que você queira verificar: ");
+        int numero = entrada.nextInt();
+
+        /**
+         * Verifica se o número coletado atende ás confições e guarda um valor lógico dentro da variável
+         * resultado, uma mensagem (determinada pelo valor lógico guardado na variável 'resultado') será
+         * retornada ao usuário classificando o número digitado entre positivo
+         * (se verdadeiro) ou negativo (se falso).
+         */
+        String resultado = (numero >= 0) ? "O número " + numero + " é positivo." : "O número " + numero + " é negativo.";
+        System.out.println(resultado);
+
+        // Fecha o Scanner e deixa de coletar dados de entrada (input) do usuário.
+        entrada.close();
+    }
+}
---
+++ Semestre1_Java/Lab4PNTernario/src/main/java/iap/lab4pnternario/Lab4PNTernario2.java
@@ -0,0 +1,30 @@
+package iap.lab4pnternario;
+// Importa o Scanner, que lê o input do usuário.
+import java.util.Scanner;
+
+public class Lab4PNTernario2 {
+    public static void main(String[] args) {
+        // Cria um novo objeto scanner com o nome 'entrada'.
+        Scanner entrada = new Scanner(System.in);
+
+        /** 
+         * Exibe uma mensagem ao usuário pedindo um número inteiro para ser verificado;
+         * Em seguida armazena o número digitado na variável 'numero'.
+         */
+        System.out.print("Digite um número inteiro que você queira verificar: ");
+        int numero = entrada.nextInt();
+
+        /**
+         * Verifica se o número coletado atende ás confições e guarda um valor lógico dentro da variável
+         * resultado, uma mensagem (determinada pelo valor lógico guardado na variável 'resultado') será
+         * retornada ao usuário classificando o número digitado entre positivo
+         * (se verdadeiro) ou negativo (se falso), também há uma mensagem que avisa se o número digitado
+         * é igual a 0 (Zero).
+         */
+        String resultado = (numero == 0) ? "O número é 0 (Zero)" : (numero > 0) ? "O número " + numero + " é positivo." : "O número " + numero + " é negativo.";
+        System.out.println(resultado);
+
+        // Fecha o Scanner e deixa de coletar dados de entrada (input) do usuário.
+        entrada.close();
+    }
+}
