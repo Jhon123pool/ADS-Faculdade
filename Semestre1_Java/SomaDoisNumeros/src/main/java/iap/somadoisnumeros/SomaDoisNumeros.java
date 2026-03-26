1917b7b71d6e0e0df2442483101e6b1052b3711b
João Vitor Nascimento <jvjndesbrava2017@hotmail.com>
2026-03-25 22:14:21 -0300

    Update ADS $(date +%d/%m/%Y)

--- Semestre1_Java/SomaDoisNumeros/src/main/java/iap/somadoisnumeros/SomaDoisNumeros.java
+++ Semestre1_Java/SomaDoisNumeros/src/main/java/iap/somadoisnumeros/SomaDoisNumeros.java
@@ -10,18 +10,18 @@ public class SomaDoisNumeros {
         Scanner entrada = new Scanner(System.in);
 
         // Pede um número inteiro ao usuário e guarda a resposta dele na variável 'a'. 
-        System. out.print("Digite o primeiro número inteiro:");
+        System. out.print("Digite o primeiro número inteiro: ");
         int a = entrada.nextInt();
 
         // Pede um segundo número inteiro ao usuário e guarda a resposta dele na variável 'b'.
-        System.out.print ("Digite o segundo número inteiro:");
+        System.out.print ("Digite o segundo número inteiro: ");
         int b = entrada.nextInt();
 
         // Soma os números guardados nas variáveis 'a' e 'b', em seguida armazena o resultado na variável 'soma'. 
         int soma = a + b;
 
         // Exibe a o resultado da soma que está armazenada na variável 'soma'.
-        System.out.printf("A soma de " + a + "e" + b, "é" + soma);
+        System.out.printf("A soma de " + a + " + " + b + " é " + soma + ". ");
 
         // Fecha o Scanner, o que é uma boa prática e evita o vazamento de memória.
         entrada.close();
