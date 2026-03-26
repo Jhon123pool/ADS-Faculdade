e192f1e24aa49407f98daa83af18ea5630c0b8d0
João Vitor Nascimento <jvjndesbrava2017@hotmail.com>
2026-03-26 14:48:59 -0300

    Update ADS $(date +%d/%m/%Y)

---
+++ Semestre1_Java/ListaExerc1/src/main/java/iap/listaexerc1/Q1.java
@@ -0,0 +1,29 @@
+package iap.listaexerc1;
+
+// Importa a ferramenta Scanner, que lê o input do usuário.
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+
+        // Cria um novo objeto Scanner chamado 'entrada'.
+        Scanner entrada = new Scanner(System.in);
+
+        // Pede um número inteiro ao usuário e guarda a resposta dele na variável 'a'. 
+        System. out.print("Digite o primeiro número inteiro: ");
+        int a = entrada.nextInt();
+
+        // Pede um segundo número inteiro ao usuário e guarda a resposta dele na variável 'b'.
+        System.out.print ("Digite o segundo número inteiro: ");
+        int b = entrada.nextInt();
+
+        // Soma os números guardados nas variáveis 'a' e 'b', em seguida armazena o resultado na variável 'soma'. 
+        int soma = a + b;
+
+        // Exibe a o resultado da soma que está armazenada na variável 'soma'.
+        System.out.printf("A soma de " + a + " + " + b + " é " + soma + ". ");
+
+        // Fecha o Scanner, o que é uma boa prática e evita o vazamento de memória.
+        entrada.close();
+    }
+}
---
+++ Semestre1_Java/ListaExerc1/src/main/java/iap/listaexerc1/Q2.java
@@ -0,0 +1,39 @@
+package iap.listaexerc1;
+
+// Importa a ferramenta Scanner, que lê o input do usuário.
+import java.util.Scanner;
+
+// Importa o Locale, que permite realizar a separação das casas decimais.
+import java.util.Locale;
+
+public class Q2 {
+    public static void main(String[] args) {
+
+        // Cria um novo objeto Scanner chamado 'entrada'.
+        Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
+
+        // Pede uma nota ao usuário e guarda a resposta dele na variável 'notaa'. 
+        System. out.print("Digite a primeira nota usando o ponto para separar as casas decimais. (Ex. 7.9): ");
+        double notaa = entrada.nextDouble();
+
+        // Pede uma segunda nota ao usuário e guarda a resposta dele na variável 'notab'.
+        System.out.print ("Digite a segunda nota usando o ponto para separar as casas decimais. (Ex. 6.7): ");
+        double notab = entrada.nextDouble();
+
+        // Pede uma terceira nota ao usuário e guarda a resposta dele na variável 'notac'.
+        System.out.print ("Digite a terceira nota usando o ponto para separar as casas decimais (Ex. 8.5): ");
+        double notac = entrada.nextDouble();
+
+        /** 
+         * Soma os números guardados nas variáveis 'notaa', 'notab' e 'notac',
+         *  em seguida divide o resultado por 3 e  armazena o resultado da divisão na variável 'media'.
+         */
+        double media = (notaa + notab + notac) / 3;
+
+        // Exibe a o resultado da média do usuário que está armazenada na variável 'media'.
+        System.out.printf("A sua média é " + media + ".");
+
+        // Fecha o Scanner, o que é uma boa prática e evita o vazamento de memória.
+        entrada.close();
+    }
+}
