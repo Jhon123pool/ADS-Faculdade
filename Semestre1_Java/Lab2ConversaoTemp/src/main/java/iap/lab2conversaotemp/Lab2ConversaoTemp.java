7c1caa0ec5ead528a4c08da2b222ad823f367116
João Vitor Nascimento <jvjndesbrava2017@hotmail.com>
2026-03-31 19:32:48 -0300

    Update ADS $(date +%d/%m/%Y)

--- Semestre1_Java/Lab2ConversaoTemp/src/main/java/iap/lab2conversaotemp/Lab2ConversaoTemp.java
+++ Semestre1_Java/Lab2ConversaoTemp/src/main/java/iap/lab2conversaotemp/Lab2ConversaoTemp.java
@@ -1,22 +1,35 @@
 package iap.lab2conversaotemp;
+
+// Importa o Scanner, que lê o input do usuário.
 import java.util.Scanner;
-import java.util.Locale; /* Primeiro importamos as ferramentas Scanner (que lê os
-                          * dados inseridos pelo teclado) e Locale (que reconhece
-                          * números com casas decimais). */
+
+// Importa o Locale, que reconhece números com casas decimais.
+import java.util.Locale;
 
 public class Lab2ConversaoTemp {
     public static void main(String[] args) {
+
+        /** 
+         * Cria um objeto Scanner chamado 'entrada' e configura o Locale
+         * para o padrão americano que reconhece o ponto '.' como separador
+         * das casas decimais.
+         */
         Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
-        /* Com o comando acima, definimos que o "." será utilizado para separar as
-         * casas decimais (seguindo o padrão americano). */
-        System.out.println("Digite um valor de temperatura em Fahrenheit que você queira converter para Celsius: ");
+
+        System.out.print("Digite um valor de temperatura em Fahrenheit que você queira converter para Celsius: ");
+
+        /** 
+         * Define uma variável para armazenar o valor digitado pelo usuário chamada 'fahrenheit' e 
+         * no comando seguinte executa o cálculo de conversão para Celsius e guarda o resultado na 
+         * variável 'celsius'. 
+         * */
         double fahrenheit = entrada.nextDouble (); 
         double celsius = (5.0/9.0) * (fahrenheit - 32);
-        /* Com os dois últimos comandos, definimos uma variável para armazenar o
-         * valor digitado pelo usuário chamada de fahrenheit e no comando seguinte
-         * executamos o cálculo de conversão para Celsius e guardamos o resultado. */
+
+        // Exibe o resultado do cálculo que definimos na variável "celsius".
         System.out.printf("Temperatura em Celsius: %.2f", celsius);
-        /* Como último comando, o programa exibe o resultado do cálculo que definimos
-         * na variável "celsius". */
+
+        // Fecha o Scanner, que deixa de ler o input do usuário.
+        entrada.close();
     }
 }
